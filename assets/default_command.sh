11a3c72b06b28599f9e0bf7e5726824e74dad07b
Hector Rivas Gandara <keymon@gmail.com>
2017-06-12 17:59:20 +0100

    Add new line before final echo

--- assets/default_command.sh
+++ assets/default_command.sh
@@ -4,7 +4,7 @@ set -e
 
 SCRIPTS_PATH=${SCRIPTS_PATH:-/usr/local/bin}
 
-trap 'echo "Backup job finished with exit code: $?"'  EXIT INT TERM
+trap 'echo -e "\nBackup job finished with exit code: $?"'  EXIT INT TERM
 
 "${SCRIPTS_PATH}"/dump_database.sh
 "${SCRIPTS_PATH}"/sync_to_s3.sh
